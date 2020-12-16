63a7ae33462a6d2fa9cedfc25c13e6890a83a1b0
ipetrash <ilya.petrash@inbox.ru>
2020-12-16 18:56:42 +0500

    Added tools/run_all_parsers.py

---
+++ tools/run_all_parsers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import subprocess
+import sys
+
+from pathlib import Path
+
+
+DIR = Path(__file__).resolve().parent
+DIR_PARSERS = DIR / 'parsers'
+
+
+for file_name in DIR_PARSERS.glob('main__*.fb2.py'):
+    file_name = str(file_name)
+    print('[RUN]:', file_name)
+
+    subprocess.run([sys.executable, file_name])
+
+    print('\n' + '-' * 100 + '\n')
