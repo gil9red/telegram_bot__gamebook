dbcdfe9da91fe405451212034f58855a7ae3f844
ipetrash <ilya.petrash@inbox.ru>
2020-12-18 05:20:57 +0500

    Added tools/parsers/main__Stayn_Dnevnik_sumasshedshey_mumii_RuLit_Net_fb2.py

---
+++ tools/parsers/main__Stayn_Dnevnik_sumasshedshey_mumii_RuLit_Net_fb2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+
+from tools.parsers.main__noch_v_lesu_oborotnej_RuLiter_Ru_12541_fb2 import parse_fb2
+from tools.parsers.utils import get_transitions
+
+
+DIR = Path(__file__).resolve().parent
+file_name = DIR / 'Ужастики-2' / 'Stayn_Dnevnik_sumasshedshey_mumii_RuLit_Net.fb2'
+
+# Страницы, в которых нужно монету подбрасывать
+COIN_FLIP = [
+    '15', '35', '69'
+]
+
+
+if __name__ == '__main__':
+    parse_fb2(
+        file_name,
+        lambda tags: get_transitions(tags, lambda tag: tag.p),
+        COIN_FLIP
+    )
